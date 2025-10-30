9a28933ea8f4a38dd9ebeb99b4a2280f4a02fbc9
Jorge Tinoco <jorge.tinoco.kfc>
2025-10-30 10:42:16 -0500

    Database structure completed

--- db/schema.sql
+++ db/schema.sql
@@ -6,4 +6,62 @@ CREATE TABLE IF NOT EXISTS customers (
   phone        VARCHAR(20),
   status       BOOLEAN                  DEFAULT 1,
   INDEX index_id_email (email)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- Orders table
+CREATE TABLE IF NOT EXISTS orders (
+  id          INT AUTO_INCREMENT                          PRIMARY KEY,
+  customer_id INT                                         NOT NULL,
+  total_cents DECIMAL(10,2)                               NOT NULL DEFAULT 0,
+  created_at  TIMESTAMP                                   DEFAULT CURRENT_TIMESTAMP,
+  status      ENUM('CREATED', 'CONFIRMED', 'CANCELED')    DEFAULT 'CREATED',
+  -- Clave foránea (relación con customers.id)
+  CONSTRAINT fk_orders_customers
+      FOREIGN KEY (customer_id)
+      REFERENCES customers(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- Products table
+CREATE TABLE IF NOT EXISTS products (
+  id          INT                     AUTO_INCREMENT PRIMARY KEY,
+  sku         VARCHAR(50)             NOT NULL UNIQUE,
+  name        VARCHAR(150)            NOT NULL,
+  price_cents DECIMAL(10,2)           NOT NULL,
+  stock       INT                     DEFAULT 0,
+  created_at  TIMESTAMP               DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- Crear tabla order_items si no existe
+CREATE TABLE IF NOT EXISTS order_items (
+  id                INT             AUTO_INCREMENT PRIMARY KEY,
+  order_id          INT             NOT NULL,
+  product_id        INT             NOT NULL,
+  qty               INT             NOT NULL,
+  unit_price_cents  DECIMAL(10,2)   NOT NULL,
+  subtotal_cents    DECIMAL(10,2)   NOT NULL,
+  -- Foreign keys order
+  CONSTRAINT fk_order_items_order
+      FOREIGN KEY (order_id)
+      REFERENCES orders(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+  -- Foreign keys product
+  CONSTRAINT fk_order_items_product
+      FOREIGN KEY (product_id)
+      REFERENCES products(id)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- Crear tabla idempotency_keys si no existe
+CREATE TABLE IF NOT EXISTS idempotency_keys (
+  key_id          VARCHAR(100)                            PRIMARY KEY,
+  target_type     VARCHAR(50)                             NOT NULL,
+  target_id       INT UNSIGNED                            NOT NULL,
+  status          ENUM('PENDING', 'COMPLETED', 'FAILED')  DEFAULT 'PENDING',
+  response_body   TEXT,
+  created_at      TIMESTAMP                               DEFAULT CURRENT_TIMESTAMP,
+  expires_at      TIMESTAMP                               NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
--- Insert data fake to customers table
+-- Insert datafake customers table
 INSERT INTO customers (name, email, phone, status) VALUES
 ('Juan Perez', 'juan.perez@example.com', '+593987654321', 1),
 ('Maria Lopez', 'maria.lopez@example.com', '+593998877665', 1),
@@ -10,3 +10,30 @@ INSERT INTO customers (name, email, phone, status) VALUES
 ('Laura Sánchez', 'laura.sanchez@example.com', '+593988877766', 1),
 ('Diego Castillo', 'diego.castillo@example.com', '+593977665544', 1),
 ('Valeria Cruz', 'valeria.cruz@example.com', '+593966554433', 1);
+
+-- Insert datafake products table
+INSERT INTO products (sku, name, price_cents, stock) VALUES
+('SKU-001', 'Hamburguesa Clásica', 599, 50),
+('SKU-002', 'Papas Fritas Grandes', 299, 100),
+('SKU-003', 'Refresco 500ml', 199, 200),
+('SKU-004', 'Combo Familiar', 1999, 30),
+('SKU-005', 'Helado de Vainilla', 249, 75);
+
+-- Insert datafake idempotency table
+INSERT INTO idempotency_keys (key_id, target_type, target_id, status, response_body, expires_at) VALUES
+('abc123', 'order', 1, 'COMPLETED', '{"message": "Order created"}', DATE_ADD(NOW(), INTERVAL 1 DAY)),
+('xyz789', 'order', 2, 'PENDING', NULL, DATE_ADD(NOW(), INTERVAL 2 DAY)),
+('uuid-0001', 'payment', 5, 'FAILED', '{"error": "Insufficient funds"}', DATE_ADD(NOW(), INTERVAL 3 DAY));
+
+-- Insert datafake orders table
+INSERT INTO order (id, customer_id, total_cents, create_at, status) VALUES
+(1, 2, 1497, NOW(), 'CREATED')
+(2, 3, 199, NOW(), 'CONFIRMED')
+(3, 4, 1999, NOW(), 'CANCEL')
+
+-- Insert datafake order_items table
+INSERT INTO order_items (order_id, product_id, qty, unit_price_cents, subtotal_cents) VALUES
+(1, 1, 2, 599, 1198),
+(1, 2, 1, 299, 299),
+(2, 3, 3, 199, 597),
+(3, 4, 1, 1999, 1999);
