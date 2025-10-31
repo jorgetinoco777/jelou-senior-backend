19e80b09e85084026f58ad536fd4a74047096205
Jorge Tinoco <jorge.tinoco.kfc>
2025-10-30 23:57:43 -0500

    Lambda orchestrator schema validator implemented

--- lambda-orchestrator/handler.js
+++ lambda-orchestrator/handler.js
@@ -2,6 +2,29 @@ const serverless = require("serverless-http");
 const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
+const Joi = require("joi");
+
+const ProductsOrderSchema = Joi.object({
+  product_id: Joi.number().required(),
+  qty: Joi.number().required(),
+});
+
+const OrchestratorSchema = Joi.object({
+  customer_id: Joi.number().required(),
+  items: Joi.array().items(ProductsOrderSchema).min(1).required(),
+  idempotency_key: Joi.string().min(1).required()
+});
+
+const validateSchema = (req, res, next) => {
+  const { error } = OrchestratorSchema.validate(req.body);
+
+  if (error)
+    return res.status(400).json({
+      message: error.message,
+    });
+
+  next();
+};
 
 const app = express();
 
@@ -12,81 +35,86 @@ app.use(bodyParser.urlencoded({ extended: true })); // Parses URL-encoded data
 //const OrchestratorSchema = require("./validators.js");
 
 // --- Configuración global ---
-const CUSTOMERS_API = process.env.CUSTOMERS_API || "http://localhost:3001";
-const ORDERS_API = process.env.CUSTOMERS_API || "http://localhost:3002";
-
-const TIMEOUT = "10000";
+const CUSTOMERS_API = process.env.CUSTOMERS_API_BASE || "http://localhost:3001";
+const ORDERS_API = process.env.CUSTOMERS_API_BASE || "http://localhost:3002";
 
 app.use(express.json());
 
-app.post("/orchestrator/create-and-confirm-order", async (req, res, next) => {
-  const { customerId } = req.body;
+app.post(
+  "/orchestrator/create-and-confirm-order",
+  validateSchema,
+  async (req, res, next) => {
+    const { customer_id: customerId } = req.body;
 
-  try {
-    console.log("----- 1. Valid customer -----");
+    OrchestratorSchema.validate(req.body);
 
-    // 1.1. Authentication: Get jwt token
-    const { data: userAuthentication } = await axios.post(
-      `${CUSTOMERS_API}/auth/login`,
-      {
-        email: "jelou@test.com",
-        password: "jelou1234",
-      },
-      {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
+    try {
+      console.log("----- 1. Valid customer -----");
+
+      // 1.1. Authentication: Get jwt token
+      const { data: userAuthentication } = await axios.post(
+        `${CUSTOMERS_API}/auth/login`,
+        {
+          email: "jelou@test.com",
+          password: "jelou1234",
         },
-      }
-    );
-    console.log("JWT: ", userAuthentication.jwt);
-
-    // 1.2. Validate customer
-    const { data: customer } = await axios.get(
-      `${CUSTOMERS_API}/internal/customers/${customerId}`,
-      {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${userAuthentication.jwt}`,
-        },
-      }
-    );
-    console.log("Customer: ", customer);
-
-    console.log("----- 2. Create order -----");
-    const { data: currentOrder } = await axios.post(
-      `${ORDERS_API}/orders`,
-      {
-        customer_id: 2,
-        items: [
-          {
-            product_id: 1,
-            qty: 10,
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
           },
-        ],
-      },
-      {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
+        }
+      );
+      // console.log("JWT: ", userAuthentication.jwt);
+      // console.log("id: ", customerId);
+
+      // 1.2. Validate customer
+      const { data: customer } = await axios.get(
+        `${CUSTOMERS_API}/internal/customers/${customerId}`,
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${userAuthentication.jwt}`,
+          },
+        }
+      );
+      // console.log("Customer: ", customer);
+
+      console.log("----- 2. Create order -----");
+      const { data: currentOrder } = await axios.post(
+        `${ORDERS_API}/orders`,
+        {
+          customer_id: 2,
+          items: [
+            {
+              product_id: 1,
+              qty: 10,
+            },
+          ],
         },
-      }
-    );
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-    console.log("Order: ", currentOrder);
+      // console.log("Order: ", currentOrder);
 
-    return res.status(200).json({
-      success: true,
-      data: {
-        customer,
-        order: { ...currentOrder },
-      },
-    });
-  } catch (error) {
-    console.error("Error: ", error);
-    res.status(500).json({ error: "Failed to fetch data" });
+      return res.status(200).json({
+        success: true,
+        data: {
+          customer,
+          order: { ...currentOrder },
+        },
+      });
+    } catch (error) {
+      console.error("Error: ", error);
+      res.status(500).json({ error: "Failed to fetch data" });
+    }
   }
-});
+);
 
 exports.handler = serverless(app);
