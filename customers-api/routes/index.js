610e85b16b42b498647b12301cbd5e21890ba6cf
Jorge Tinoco <jorge.tinoco.kfc>
2025-10-31 00:31:13 -0500

    Service health implemented

--- customers-api/routes/index.js
+++ customers-api/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import customersRoutes from "./customers.routes.js";
 import internalRoutes from "./internal.routes.js";
 import authRoutes from "./auth.routes.js";
+import healthRoutes from "./health.routes.js";
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.use("/customers", customersRoutes);
 router.use("/internal", internalRoutes);
 
 router.use("/auth", authRoutes);
+
+router.use("/health", healthRoutes);
 
 export default router;
--- orders-api/routes/index.js
+++ orders-api/routes/index.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import orderRoutes from "./order.routes.js";
 import productRoutes from "./product.routes.js";
+import healthRoutes from "./health.routes.js";
 
 const router = Router();
 
 router.use("/orders", orderRoutes);
 
 router.use("/products", productRoutes);
+
+router.use("/health", healthRoutes);
 
 export default router;
